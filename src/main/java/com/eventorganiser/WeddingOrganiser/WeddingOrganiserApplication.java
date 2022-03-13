0814d07b161d84c69c23b9dadc2367c821760084
nehaguptadev <gnehagupta101@gmail.com>
2022-03-13 06:56:15 +0530

    Code Setup

---
+++ src/main/java/com/eventorganiser/WeddingOrganiser/WeddingOrganiserApplication.java
@@ -0,0 +1,13 @@
+package com.eventorganiser.WeddingOrganiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingOrganiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventorganiser/WeddingOrganiser/WeddingOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventorganiser.WeddingOrganiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingOrganiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
