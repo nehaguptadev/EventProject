05199eeaedb6ea1a683a5dc7e47c9407bd5647f1
dev-akshat-gupta <akshatgupta1412@gmail.com>
2022-03-14 12:29:37 +0530

    Added a comment

--- src/main/java/com/eventorganiser/WeddingOrganiser/WeddingOrganiserApplication.java
+++ src/main/java/com/eventorganiser/WeddingOrganiser/WeddingOrganiserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WeddingOrganiserApplication {
 
+	//Main Class
 	public static void main(String[] args) {
 		SpringApplication.run(WeddingOrganiserApplication.class, args);
 	}
